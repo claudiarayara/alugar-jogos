b5b2e01bb691eee3e3d8a84f34897cb74ba099e9
Claudia Rayara <claudia_rayara@hotmail.com>
2024-05-01 13:47:00 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,33 @@
+let jogosAlugados = 0;
+
+function contarJogosAlugados() {
+    console.log(`Total de jogos alugados: ${jogosAlugados}`);
+}
+
+function alterarStatus(id) {
+    let gameSelecionado = document.getElementById(`game-${id}`);
+    let imagem = gameSelecionado.querySelector('.dashboard__item__img');
+    let botao = gameSelecionado.querySelector('.dashboard__item__button');
+    let nomeJogo = gameSelecionado.querySelector('.dashboard__item__name');
+
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        if (confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo.textContent}?`)) {
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+            botao.textContent = 'Alugar';
+            jogosAlugados--;
+        }
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
+        jogosAlugados++;
+    }
+
+    contarJogosAlugados();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+    contarJogosAlugados();
+});
